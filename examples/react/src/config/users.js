2d156d486897adee277dff4219ce38c65ebef3fb
QSD_k <info@qsdbih.com>
2019-04-18 20:31:45 +0200

    Add style

--- examples/react/src/config/users.js
+++ examples/react/src/config/users.js
@@ -1,50 +1,76 @@
+import avatars from './avatars';
+
 export default [
     {
         uuid: 'u-00000',
         firstName: 'Finny',
         lastName: 'Fish',
-        profileImage: ''
+        profileImage: {
+            lgImage: avatars.finnie_45px,
+            smImage: avatars.finnie_28px
+        }
     },
     {
         uuid: 'u-00001',
         firstName: 'Daniel',
         lastName: 'Dog',
-        profileImage: ''
+        profileImage: {
+            lgImage: avatars.daniel_45px,
+            smImage: avatars.daniel_28px
+        }
     },
     {
         uuid: 'u-00002',
         firstName: 'Bernie',
         lastName: 'Bear',
-        profileImage: ''
+        profileImage: {
+            lgImage: avatars.bernie_45px,
+            smImage: avatars.bernie_28px
+        }
     },
     {
         uuid: 'u-00003',
         firstName: 'Carl',
         lastName: 'Cat',
-        profileImage: ''
+        profileImage: {
+            lgImage: avatars.carl_45px,
+            smImage: avatars.carl_28px
+        }
     },
     {
         uuid: 'u-00004',
         firstName: 'Uri',
         lastName: 'Unicorn',
-        profileImage: ''
+        profileImage: {
+            lgImage: avatars.uri_45px,
+            smImage: avatars.uri_28px
+        }
     },
     {
         uuid: 'u-00005',
         firstName: 'Monty',
         lastName: 'Monkey',
-        profileImage: ''
+        profileImage: {
+            lgImage: avatars.monty_45px,
+            smImage: avatars.monty_28px
+        }
     },
     {
         uuid: 'u-00006',
         firstName: 'Ollie',
         lastName: 'Owl',
-        profileImage: ''
+        profileImage: {
+            lgImage: avatars.ollie_45px,
+            smImage: avatars.ollie_28px
+        }
     },
     {
         uuid: 'u-00007',
         firstName: 'Larry',
         lastName: 'Lion',
-        profileImage: ''
+        profileImage: {
+            lgImage: avatars.larry_45px,
+            smImage: avatars.larry_28px
+        }
     }
 ]
