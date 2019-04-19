7604fc1393c999766ec7a32707e1487ade0f5493
Serhii Mamontov <parfeon@me.com>
2019-04-20 00:13:26 +0300

    feat: add more user profiles (#5)
    Added more pre-defined user profiles with avatar images.

--- examples/react/src/config/avatars.js
+++ examples/react/src/config/avatars.js
@@ -1,36 +1,84 @@
-import bernie_45px from '../styles/avatars/45px/bernie.png';
-import carl_45px from '../styles/avatars/45px/carl.png';
-import daniel_45px from '../styles/avatars/45px/daniel.png';
-import finnie_45px from '../styles/avatars/45px/finnie.png';
-import larry_45px from '../styles/avatars/45px/larry.png';
-import monty_45px from '../styles/avatars/45px/monty.png';
-import ollie_45px from '../styles/avatars/45px/ollie.png';
-import uri_45px from '../styles/avatars/45px/uri.png';
+import baby_deer_45px from '../styles/avatars/45px/baby-deer.png';
+import blue_bird_45px from '../styles/avatars/45px/blue-bird.png';
+import colorful_cock_45px from '../styles/avatars/45px/colorful-cock.png';
+import cute_bear_45px from '../styles/avatars/45px/cute-bear.png';
+import forest_cat_45px from '../styles/avatars/45px/forest-cat.png';
+import funky_monkey_45px from '../styles/avatars/45px/funky-monkey.png';
+import happy_turtle_45px from '../styles/avatars/45px/happy-turtle.png';
+import happy_chipmunk_45px from '../styles/avatars/45px/happy-chimpunk.png';
+import jungle_tiger_45px from '../styles/avatars/45px/jungle-tiger.png';
+import king_lion_45px from '../styles/avatars/45px/king-lion.png';
+import little_elephant_45px from '../styles/avatars/45px/little-elephant.png';
+import long_giraffe_45px from '../styles/avatars/45px/long-giraffe.png';
+import night_owl_45px from '../styles/avatars/45px/night-owl.png';
+import parrot_arra_45px from '../styles/avatars/45px/parrot-arra.png';
+import safari_camel_45px from '../styles/avatars/45px/safari-camel.png';
+import sleeping_cheetah_45px from '../styles/avatars/45px/sleeping-cheetah.png';
+import striped_zebra_45px from '../styles/avatars/45px/striped-zebra.png';
+import tropical_toucan_45px from '../styles/avatars/45px/tropical-toucan.png';
+import white_rabbit_45px from '../styles/avatars/45px/white-rabbit.png';
+import wild_eagle_45px from '../styles/avatars/45px/wild-eagle.png';
 
-import bernie_28px from '../styles/avatars/28px/bernie.png';
-import carl_28px from '../styles/avatars/28px/carl.png';
-import daniel_28px from '../styles/avatars/28px/daniel.png';
-import finnie_28px from '../styles/avatars/28px/finnie.png';
-import larry_28px from '../styles/avatars/28px/larry.png';
-import monty_28px from '../styles/avatars/28px/monty.png';
-import ollie_28px from '../styles/avatars/28px/ollie.png';
-import uri_28px from '../styles/avatars/28px/uri.png';
+import baby_deer_28px from '../styles/avatars/28px/baby-deer.png';
+import blue_bird_28px from '../styles/avatars/28px/blue-bird.png';
+import colorful_cock_28px from '../styles/avatars/28px/colorful-cock.png';
+import cute_bear_28px from '../styles/avatars/28px/cute-bear.png';
+import forest_cat_28px from '../styles/avatars/28px/forest-cat.png';
+import funky_monkey_28px from '../styles/avatars/28px/funky-monkey.png';
+import happy_turtle_28px from '../styles/avatars/28px/happy-turtle.png';
+import happy_chipmunk_28px from '../styles/avatars/28px/happy-chimpunk.png';
+import jungle_tiger_28px from '../styles/avatars/28px/jungle-tiger.png';
+import king_lion_28px from '../styles/avatars/28px/king-lion.png';
+import little_elephant_28px from '../styles/avatars/28px/little-elephant.png';
+import long_giraffe_28px from '../styles/avatars/28px/long-giraffe.png';
+import night_owl_28px from '../styles/avatars/28px/night-owl.png';
+import parrot_arra_28px from '../styles/avatars/28px/parrot-arra.png';
+import safari_camel_28px from '../styles/avatars/28px/safari-camel.png';
+import sleeping_cheetah_28px from '../styles/avatars/28px/sleeping-cheetah.png';
+import striped_zebra_28px from '../styles/avatars/28px/striped-zebra.png';
+import tropical_toucan_28px from '../styles/avatars/28px/tropical-toucan.png';
+import white_rabbit_28px from '../styles/avatars/28px/white-rabbit.png';
+import wild_eagle_28px from '../styles/avatars/28px/wild-eagle.png';
 
 export default {
-    bernie_45px,
-    carl_45px,
-    daniel_45px,
-    finnie_45px,
-    larry_45px,
-    monty_45px,
-    ollie_45px,
-    uri_45px,
-    bernie_28px,
-    carl_28px,
-    daniel_28px,
-    finnie_28px,
-    larry_28px,
-    monty_28px,
-    ollie_28px,
-    uri_28px
+    baby_deer_45px,
+    blue_bird_45px,
+    colorful_cock_45px,
+    cute_bear_45px,
+    forest_cat_45px,
+    funky_monkey_45px,
+    happy_chipmunk_45px,
+    happy_turtle_45px,
+    jungle_tiger_45px,
+    king_lion_45px,
+    little_elephant_45px,
+    long_giraffe_45px,
+    night_owl_45px,
+    parrot_arra_45px,
+    safari_camel_45px,
+    sleeping_cheetah_45px,
+    striped_zebra_45px,
+    tropical_toucan_45px,
+    white_rabbit_45px,
+    wild_eagle_45px,
+    baby_deer_28px,
+    blue_bird_28px,
+    colorful_cock_28px,
+    cute_bear_28px,
+    forest_cat_28px,
+    funky_monkey_28px,
+    happy_chipmunk_28px,
+    happy_turtle_28px,
+    jungle_tiger_28px,
+    king_lion_28px,
+    little_elephant_28px,
+    long_giraffe_28px,
+    night_owl_28px,
+    parrot_arra_28px,
+    safari_camel_28px,
+    sleeping_cheetah_28px,
+    striped_zebra_28px,
+    tropical_toucan_28px,
+    white_rabbit_28px,
+    wild_eagle_28px
 };
--- examples/react/src/config/users.js
+++ examples/react/src/config/users.js
@@ -2,75 +2,203 @@ import avatars from './avatars';
 
 export default [
     {
-        uuid: 'u-00000',
-        firstName: 'Finny',
-        lastName: 'Fish',
-        profileImage: {
-            lgImage: avatars.finnie_45px,
-            smImage: avatars.finnie_28px
-        }
-    },
-    {
-        uuid: 'u-00001',
-        firstName: 'Daniel',
-        lastName: 'Dog',
-        profileImage: {
-            lgImage: avatars.daniel_45px,
-            smImage: avatars.daniel_28px
-        }
-    },
-    {
-        uuid: 'u-00002',
-        firstName: 'Bernie',
-        lastName: 'Bear',
-        profileImage: {
-            lgImage: avatars.bernie_45px,
-            smImage: avatars.bernie_28px
-        }
-    },
-    {
-        uuid: 'u-00003',
-        firstName: 'Carl',
-        lastName: 'Cat',
-        profileImage: {
-            lgImage: avatars.carl_45px,
-            smImage: avatars.carl_28px
-        }
-    },
-    {
-        uuid: 'u-00004',
-        firstName: 'Uri',
-        lastName: 'Unicorn',
-        profileImage: {
-            lgImage: avatars.uri_45px,
-            smImage: avatars.uri_28px
-        }
-    },
-    {
-        uuid: 'u-00005',
-        firstName: 'Monty',
+        uuid: 'user-1',
+        firstName: 'Funky',
         lastName: 'Monkey',
+        designation: 'Technical Specialist',
         profileImage: {
-            lgImage: avatars.monty_45px,
-            smImage: avatars.monty_28px
+            lgImage: avatars.funky_monkey_45px,
+            smImage: avatars.funky_monkey_28px
         }
     },
     {
-        uuid: 'u-00006',
-        firstName: 'Ollie',
-        lastName: 'Owl',
+        uuid: 'user-2',
+        firstName: 'Parrot',
+        lastName: 'Arra',
+        designation: 'Personal Assistant',
         profileImage: {
-            lgImage: avatars.ollie_45px,
-            smImage: avatars.ollie_28px
+            lgImage: avatars.parrot_arra_45px,
+            smImage: avatars.parrot_arra_28px
         }
     },
     {
-        uuid: 'u-00007',
-        firstName: 'Larry',
+        uuid: 'user-3',
+        firstName: 'Happy',
+        lastName: 'Turtle',
+        designation: 'Account Manager',
+        profileImage: {
+            lgImage: avatars.happy_turtle_45px,
+            smImage: avatars.happy_turtle_28px
+        }
+    },
+    {
+        uuid: 'user-4',
+        firstName: 'Sleeping',
+        lastName: 'Cheetah',
+        designation: 'Product Manager',
+        profileImage: {
+            lgImage: avatars.sleeping_cheetah_45px,
+            smImage: avatars.sleeping_cheetah_28px
+        }
+    },
+    {
+        uuid: 'user-5',
+        firstName: 'Happy',
+        lastName: 'Chipmunk',
+        designation: 'Dev-Ops Engineer',
+        profileImage: {
+            lgImage: avatars.happy_chipmunk_45px,
+            smImage: avatars.happy_chipmunk_28px
+        }
+    },
+    {
+        uuid: 'user-6',
+        firstName: 'Long',
+        lastName: 'Giraffe',
+        designation: 'Technical Specialist',
+        profileImage: {
+            lgImage: avatars.long_giraffe_45px,
+            smImage: avatars.long_giraffe_28px
+        }
+    },
+    {
+        uuid: 'user-7',
+        firstName: 'Little',
+        lastName: 'Elephant',
+        designation: 'Sales Director',
+        profileImage: {
+            lgImage: avatars.little_elephant_45px,
+            smImage: avatars.little_elephant_28px
+        }
+    },
+    {
+        uuid: 'user-8',
+        firstName: 'Blue',
+        lastName: 'Bird',
+        designation: 'VP Marketing',
+        profileImage: {
+            lgImage: avatars.blue_bird_45px,
+            smImage: avatars.blue_bird_28px
+        }
+    },
+    {
+        uuid: 'user-9',
+        firstName: 'Colorful',
+        lastName: 'Rooster',
+        designation: 'Network Engineer',
+        profileImage: {
+            lgImage: avatars.colorful_cock_45px,
+            smImage: avatars.colorful_cock_28px
+        }
+    },
+    {
+        uuid: 'user-10',
+        firstName: 'Tropical',
+        lastName: 'Toucan',
+        designation: 'Sales Manager',
+        profileImage: {
+            lgImage: avatars.tropical_toucan_45px,
+            smImage: avatars.tropical_toucan_28px
+        }
+    },
+    {
+        uuid: 'user-11',
+        firstName: 'Cute',
+        lastName: 'Bear',
+        designation: 'Designer',
+        profileImage: {
+            lgImage: avatars.cute_bear_45px,
+            smImage: avatars.cute_bear_28px
+        }
+    },
+    {
+        uuid: 'user-12',
+        firstName: 'White',
+        lastName: 'Rabbit',
+        designation: 'Network Engineer',
+        profileImage: {
+            lgImage: avatars.white_rabbit_45px,
+            smImage: avatars.white_rabbit_28px
+        }
+    },
+    {
+        uuid: 'user-13',
+        firstName: 'King',
         lastName: 'Lion',
+        designation: 'Co-Founder',
         profileImage: {
-            lgImage: avatars.larry_45px,
-            smImage: avatars.larry_28px
+            lgImage: avatars.king_lion_45px,
+            smImage: avatars.king_lion_28px
+        }
+    },
+    {
+        uuid: 'user-14',
+        firstName: 'Night',
+        lastName: 'Owl',
+        designation: 'VP Finance',
+        profileImage: {
+            lgImage: avatars.night_owl_45px,
+            smImage: avatars.night_owl_28px
+        }
+    },
+    {
+        uuid: 'user-15',
+        firstName: 'Forest',
+        lastName: 'Cat',
+        designation: 'Designer',
+        profileImage: {
+            lgImage: avatars.forest_cat_45px,
+            smImage: avatars.forest_cat_28px
+        }
+    },
+    {
+        uuid: 'user-16',
+        firstName: 'Striped',
+        lastName: 'Zebra',
+        designation: 'VP Sales',
+        profileImage: {
+            lgImage: avatars.striped_zebra_45px,
+            smImage: avatars.striped_zebra_28px
+        }
+    },
+    {
+        uuid: 'user-17',
+        firstName: 'Jungle',
+        lastName: 'Tiger',
+        designation: 'CEO',
+        profileImage: {
+            lgImage: avatars.jungle_tiger_45px,
+            smImage: avatars.jungle_tiger_28px
+        }
+    },
+    {
+        uuid: 'user-18',
+        firstName: 'Baby',
+        lastName: 'Deer',
+        designation: 'Marketing Specialist',
+        profileImage: {
+            lgImage: avatars.baby_deer_45px,
+            smImage: avatars.baby_deer_28px
+        }
+    },
+    {
+        uuid: 'user-19',
+        firstName: 'Wild',
+        lastName: 'Eagle',
+        designation: 'Support Rep',
+        profileImage: {
+            lgImage: avatars.wild_eagle_45px,
+            smImage: avatars.wild_eagle_28px
+        }
+    },
+    {
+        uuid: 'user-20',
+        firstName: 'Safari',
+        lastName: 'Camel',
+        designation: 'Solution Architect',
+        profileImage: {
+            lgImage: avatars.safari_camel_45px,
+            smImage: avatars.safari_camel_28px
         }
     }
 ]
